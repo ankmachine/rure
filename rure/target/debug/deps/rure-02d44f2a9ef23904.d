b6cbf0e4659ecfc2bbb00890fb733603c522cb34
Ankit Sinha <none>
2019-08-31 22:45:13 +1000

    first build commit

---
+++ rure/target/debug/deps/rure-02d44f2a9ef23904.d
@@ -0,0 +1,5 @@
+/Users/ankitsinha/Documents/codeBase/rure/rure/target/debug/deps/rure-02d44f2a9ef23904: src/main.rs
+
+/Users/ankitsinha/Documents/codeBase/rure/rure/target/debug/deps/rure-02d44f2a9ef23904.d: src/main.rs
+
+src/main.rs:
---
+++ rure/target/debug/rure.d
@@ -0,0 +1,1 @@
+/Users/ankitsinha/Documents/codeBase/rure/rure/target/debug/rure.dSYM: /Users/ankitsinha/Documents/codeBase/rure/rure/src/main.rs
