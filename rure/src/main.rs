3375843be374bf1bb51ba7ca55dde0e86efb3739
Ankit Sinha <none>
2019-09-02 21:54:04 +1000

    added png output

--- rure/src/main.rs
+++ rure/src/main.rs
@@ -1,6 +1,9 @@
+extern crate image;
+use image::RgbImage;
 fn main() {
 	let nx = 200;
 	let ny = 100;
+	let mut img = RgbImage::new(nx, ny);
 	println!("P3");
 	println!("{} {}",nx,ny);
 	println!("255");
@@ -9,10 +12,15 @@ fn main() {
 			let r = i as f32/nx as f32;
 			let g = j as f32/ny as f32;
 			let b = 0.2;
+			let pixel = &mut img[(i, j)];
 			let ir = (255.99*r) as i32;
 			let ig = (255.99*g) as i32;
 			let ib = (255.99*b) as i32;
 			println!("{} {} {}", ir, ig, ib);
+			pixel[0] = ir as u8;
+			pixel[1] = ig as u8;
+			pixel[2] = ib as u8;
 		}
 	}
+	img.save("output.png").expect("Failed to save output image");
 }
