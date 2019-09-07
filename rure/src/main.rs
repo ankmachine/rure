5aef4d80ef8834ba5321642872ff0f891ba8970f
Ankit Sinha <none>
2019-09-07 15:58:49 +1000

    added vector implementation

--- rure/src/main.rs
+++ rure/src/main.rs
@@ -1,5 +1,8 @@
 extern crate image;
 use image::RgbImage;
+mod helper;
+
+use helper::vec3::Vec3;
 fn main() {
 	let nx = 200;
 	let ny = 100;
@@ -9,14 +12,13 @@ fn main() {
 	println!("255");
 	for j in (0..ny).rev(){
 		for i in 0..nx{
-			let r = i as f32/nx as f32;
-			let g = j as f32/ny as f32;
-			let b = 0.2;
-			let pixel = &mut img[(i, j)];
-			let ir = (255.99*r) as i32;
-			let ig = (255.99*g) as i32;
-			let ib = (255.99*b) as i32;
-			println!("{} {} {}", ir, ig, ib);
+			let col = Vec3::new(i as f32/nx as f32, j as f32/ny as f32, 0.2);
+			// fix by giving proper pixel value here
+			// i and j should start from 0,0.
+			let pixel = &mut img[(i, (ny-1) - j)];
+			let ir = (255.99*col[0]) as i32;
+			let ig = (255.99*col[1]) as i32;
+			let ib = (255.99*col[2]) as i32;
 			pixel[0] = ir as u8;
 			pixel[1] = ig as u8;
 			pixel[2] = ib as u8;
